f17712812aeee38476b44f1fd84efc1430b0154c
ಠ_ಠ <easteregg@verfriemelt.org>
2020-12-16 17:52:22 +0100

    added having

---
+++ _/Database/SQL/Clause/Having.php
@@ -0,0 +1,41 @@
+<?php
+
+    namespace Wrapped\_\Database\SQL\Clause;
+
+    use \Wrapped\_\Database\Driver\DatabaseDriver;
+    use \Wrapped\_\Database\SQL\Clause\Clause;
+    use \Wrapped\_\Database\SQL\Command\CommandWrapperTrait;
+    use \Wrapped\_\Database\SQL\Expression\ExpressionItem;
+    use \Wrapped\_\Database\SQL\QueryPart;
+
+    class Having
+    extends QueryPart
+    implements Clause {
+
+        use CommandWrapperTrait;
+
+        public const CLAUSE = "HAVING %s";
+
+        private ExpressionItem $expression;
+
+        public function getWeight(): int {
+            return 52;
+        }
+
+        public function __construct( ExpressionItem $expression ) {
+
+            $wrap = $this->wrap( $expression );
+
+            $this->addChild( $wrap );
+            $this->expression = $wrap;
+        }
+
+        public function stringify( DatabaseDriver $driver = null ): string {
+
+            return sprintf(
+                static::CLAUSE,
+                $this->expression->stringify( $driver )
+            );
+        }
+
+    }
