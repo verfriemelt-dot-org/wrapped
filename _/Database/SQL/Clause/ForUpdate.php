438ecd5d5e24fe83d8f9614f00af55a768fec9ae
ಠ_ಠ <easteregg@verfriemelt.org>
2022-01-03 23:05:54 +0100

    added for update

---
+++ _/Database/SQL/Clause/ForUpdate.php
@@ -0,0 +1,35 @@
+<?php
+
+    declare(strict_types = 1);
+
+    namespace verfriemelt\wrapped\_\Database\SQL\Clause;
+
+    use \verfriemelt\wrapped\_\Database\Driver\DatabaseDriver;
+    use \verfriemelt\wrapped\_\Database\SQL\Clause\Clause;
+    use \verfriemelt\wrapped\_\Database\SQL\Command\CommandWrapperTrait;
+    use \verfriemelt\wrapped\_\Database\SQL\Expression\ExpressionItem;
+    use \verfriemelt\wrapped\_\Database\SQL\QueryPart;
+
+    class ForUpdate
+    extends QueryPart
+    implements Clause {
+
+        use CommandWrapperTrait;
+
+        public const CLAUSE = "FOR UPDATE %s";
+
+        public ExpressionItem $expression;
+
+        public function getWeight(): int {
+            return 100;
+        }
+
+        public function stringify( DatabaseDriver $driver = null ): string {
+
+            return sprintf(
+                static::CLAUSE,
+                ''
+            );
+        }
+
+    }
