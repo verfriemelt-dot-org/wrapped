8df56958e41fcb3f32b820698777887339bf2f32
ಠ_ಠ <easteregg@verfriemelt.org>
2022-02-15 06:14:34 +0100

    add simple unittest for eventDispatcher

---
+++ tests/unit/Event/EventDispatcherTest.php
@@ -0,0 +1,22 @@
+<?php namespace tests\unit\Event;
+
+
+    use PHPUnit\Framework\TestCase;
+    use verfriemelt\wrapped\_\Events\EventDispatcher;
+    use verfriemelt\wrapped\_\Events\EventInterface;
+    use verfriemelt\wrapped\_\Events\EventSubscriberInterface;
+
+    class EventDispatchterTest extends TestCase {
+
+        public function testSubscriberBeeingCalled(): void {
+
+            $mockSubscriber = $this->createMock(EventSubscriberInterface::class);
+            $mockSubscriber->expects(static::once())->method('on');
+
+            $mockEvent = $this->createMock( EventInterface::class );
+
+            $dispatcher = new EventDispatcher();
+            $dispatcher->addSubscriber( $mockSubscriber );
+            $dispatcher->dispatch($mockEvent);
+        }
+    }
