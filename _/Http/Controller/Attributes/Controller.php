917f4799dfb2b126eb8aef9644e09199951ede6c
ಠ_ಠ <easteregg@verfriemelt.org>
2024-01-26 08:46:58 +0100

    add route attribute

---
+++ _/Http/Controller/Attributes/Controller.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace verfriemelt\wrapped\_\Http\Controller\Attributes;
+
+use Attribute;
+
+#[Attribute(Attribute::TARGET_CLASS)]
+final readonly class Controller {}
