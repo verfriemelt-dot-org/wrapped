36e3a40973276b43c523292c4c15809af7f94e12
ಠ_ಠ <easteregg@verfriemelt.org>
2024-03-09 10:46:20 +0100

    better error handling

---
+++ _/Template/v2/Token/Exception/EmptyContionalExpressionException.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace verfriemelt\wrapped\_\Template\v2\Token\Exception;
+
+use verfriemelt\wrapped\_\Template\v2\TokenizerException;
+
+final class EmptyContionalExpressionException extends TokenizerException {}
